5a216bb6d050f2c447343786c2b73c5b78e42f3e
areufluk <chanayut.ja@gmail.com>
2024-08-27 01:48:59 +0000

    add columns to data source to make dataset more interesting

--- airflow/dags/scripts/generate_order.py
+++ airflow/dags/scripts/generate_order.py
@@ -37,16 +37,6 @@ def generate_random_times(num_times: int) -> list:
 
 
 def create_or_update_order(start_date: str = None, end_date: str = None):
-    PRODUCT_LIST = ['watch', 'necklace', 'bracelet', 'earrings', 'wallet']
-    PRODUCT_PRICE_DICT = {
-        'watch': 1000,
-        'necklace': 800,
-        'bracelet': 500,
-        'earrings': 1200,
-        'wallet': 1500
-    }
-    STORE_LOCATION_LIST = ['bangkok', 'phuket', 'chiang mai', 'khon kaen']
-
     MIN_ORDER_AMOUNT = 50
     MAX_ORDER_AMOUNT = 50
 
@@ -61,7 +51,35 @@ def create_or_update_order(start_date: str = None, end_date: str = None):
     # Create database connection
     sales_database = postgresql_connection()
 
-    columns = ['order_datetime', 'order_id', 'product', 'quantity', 'price', 'store_location']
+    # Query customer data
+    sales_database.execute('''
+        SELECT MIN(id), MAX(id)
+        FROM customers;
+    '''.format(start_date=start_date, end_date=end_date))
+    customers_id_range = sales_database.fetchall()[0]
+
+    # Query campaign data
+    sales_database.execute('''
+        SELECT id, discount
+        FROM campaigns;
+    '''.format(start_date=start_date, end_date=end_date))
+    campaigns = sales_database.fetchall()
+    campaign_dict = {campaign[0]: campaign[1] for campaign in campaigns}
+    campaigns_id_range = [campaigns[0][0], campaigns[-1][0]]
+
+    # Query product data
+    sales_database.execute('''
+        SELECT id, price
+        FROM products;
+    '''.format(start_date=start_date, end_date=end_date))
+    products = sales_database.fetchall()
+    product_dict = {product[0]: product[1] for product in products}
+    products_id_range = [products[0][0], products[-1][0]]
+
+    columns = [
+        'order_datetime', 'order_id', 'customer_id', 'campaign_id',
+        'product_id', 'quantity', 'gross_price', 'discount', 'net_price'
+    ]
     df_orders = pd.DataFrame(columns=columns)
 
     for date_obj in date_range_list:
@@ -83,22 +101,40 @@ def create_or_update_order(start_date: str = None, end_date: str = None):
         )
         del df_order['order_index']
 
+        # Random customer into each order
+        df_order['customer_id'] = df_order['order_id'].apply(
+            lambda row: random.randint(customers_id_range[0], customers_id_range[1])
+        )
+
         # Random product into each order
-        df_order['product'] = df_order['order_id'].apply(
-            lambda row: random.choice(PRODUCT_LIST)
+        df_order['product_id'] = df_order['order_id'].apply(
+            lambda row: random.randint(products_id_range[0], products_id_range[1])
         )
 
         # Random quantity between 1-5 and multiply to product price
         df_order['quantity'] = df_order['order_id'].apply(
             lambda row: random.randint(1, 5)
         )
-        df_order['price'] = df_order.apply(
-            lambda row: PRODUCT_PRICE_DICT[row['product']] * row['quantity'], axis=1
+        df_order['gross_price'] = df_order.apply(
+            lambda row: product_dict[row['product_id']] * row['quantity'], axis=1
         )
 
-        # Random store location into each order
-        df_order['store_location'] = df_order['order_id'].apply(
-            lambda row: random.choice(STORE_LOCATION_LIST)
+        # Random campaign into each order, get discount percentage
+        df_order['campaign_id'] = df_order['order_id'].apply(
+            lambda row: random.randint(campaigns_id_range[0], campaigns_id_range[1])
+        )
+        df_order['discount_percentage'] = df_order.apply(
+            lambda row: campaign_dict[row['campaign_id']], axis=1
+        )
+
+        # Calculate discount
+        df_order['discount'] = df_order.apply(
+            lambda row: float(row['gross_price'] * row['discount_percentage']) / 100, axis=1
+        )
+
+        # Calculate net_price from gross_price and discount
+        df_order['net_price'] = df_order.apply(
+            lambda row: float(row['gross_price']) - row['discount'], axis=1
         )
 
         if df_orders.empty:
@@ -106,10 +142,13 @@ def create_or_update_order(start_date: str = None, end_date: str = None):
         else:
             df_orders = pd.concat([df_orders, df_order], axis=0)
 
+    del df_orders['discount_percentage']
+    print(df_orders.head(50))
+
     # Query exists data to check which order should created or updated
     sales_database.execute('''
         SELECT order_id
-        FROM order_data
+        FROM orders
         WHERE order_datetime::DATE BETWEEN \'{start_date}\' AND \'{end_date}\';
     '''.format(start_date=start_date, end_date=end_date))
     df_exists = pd.DataFrame.from_records(sales_database.fetchall(), columns=['order_id'])
@@ -119,20 +158,22 @@ def create_or_update_order(start_date: str = None, end_date: str = None):
     # The 'left_only' data is new data that have to be created
     created_data = df_orders[df_orders['_merge'] == 'left_only'][columns].to_dict('records')
     
-    insert_statement = 'INSERT INTO order_data ({}) VALUES %s'.format(','.join(columns))
+    insert_statement = 'INSERT INTO orders ({}) VALUES %s'.format(','.join(columns))
     values = [[value for value in data.values()] for data in created_data]
     execute_values(sales_database, insert_statement, values)
 
     # The 'both' data is old data that exists in google sheet and have to be updated
     updated_data = df_orders[df_orders['_merge'] == 'both'][columns].to_dict('records')
     upsert_statement = '''
-        UPDATE order_data AS old_data
+        UPDATE orders AS old_data
         SET 
             order_datetime = new_data.order_datetime,
-            product = new_data.product,
+            campaign_id = new_data.campaign_id,
+            product_id = new_data.product_id,
             quantity = new_data.quantity,
-            price = new_data.price,
-            store_location = new_data.store_location
+            gross_price = new_data.gross_price,
+            discount = new_data.discount,
+            net_price = new_data.net_price
         FROM (VALUES %s) AS new_data({}) 
         WHERE new_data.order_id = old_data.order_id;
     '''.format(','.join(columns))
